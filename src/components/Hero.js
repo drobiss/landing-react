613450be18fc7485366a2acfbf94c1dcb2784e4d
drobiss <brezinadavid93@icloud.com>
2024-05-05 19:36:45 +0200

    added hero

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import "./Hero.css"
+
+const Hero = () => {
+  return <section className="hero-section">
+    <div className="hero-content-container">
+      <h1>Awesome UI Dark Template for Webflow Agency</h1>
+      <button className="btn-hero">Get in Touch</button>
+    </div>
+  </section>
+}
+
+export default Hero
